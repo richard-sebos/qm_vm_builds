ce5e94d861b648d94f76d24f74ce14a4add87fb5
Richard Chamberlain <info@sebostechnology.com>
2024-12-24 03:09:48 -0600

    Create ansible_server.sh

---
+++ Ansible/ansible_server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Variables
+VMID=300
+NAME="AnsibleController"
+ISO_STORAGE="ISOs"           # Replace with your ISO storage name
+ISO_FILE="OracleLinux-R9-U4-x86_64-dvd.iso" # Replace with your ISO filename
+DISK_STORAGE="vm_storage"    # Replace with your disk storage name
+
+## If existing, remove
+qm stop $VMID || true
+qm destroy $VMID || true
+
+# Create VM
+qm create $VMID --name $NAME --memory 4096 --cores 1 --sockets 1 --numa 0 --cpu x86-64-v2-AES --ostype l26
+qm set $VMID --net0 virtio,bridge=vmbr0,tag=20
+qm set $VMID --scsihw virtio-scsi-single 
+pvesm alloc vm_storage $VMID vm-${VMID}-disk-0 60G
+qm set $VMID --scsihw virtio-scsi-single --scsi0 vm_storage:vm-${VMID}-disk-0,iothread=1
+qm set $VMID --cdrom $ISO_STORAGE:iso/$ISO_FILE
+qm set $VMID --boot order='ide2;scsi0'
+
+# Start VM
+qm start $VMID
